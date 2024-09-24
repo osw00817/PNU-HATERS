5c06763a0847696ed11854b191098416ff2db96f
osw00817 <osw00817@naver.com>
2024-09-25 01:28:09 +0900

    ✨feat: BOJ_1935_후위 표기식2 구햔 실패

---
+++ osw00817/week03/BOJ_1935_후위표기식2.c
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int nums[27];
+char operands[100];
+int top = -1;
+
+int main() {
+	int loop, cnt = -1;
+	float tmp = 0.00;
+	char data[101];
+	scanf("%d", &loop);
+	scanf("%s", data);
+
+	for (int i = 0; i < loop; i++) {
+		int tmp;
+		scanf("%d", &tmp);
+		nums[i] = tmp;
+	}
+
+	for (int i = 0; i < strlen(data); i++) {
+		printf("%s의%d번째값: %c %d | %d \n", data, i, data[i], data[i], tmp);
+		if (65 <= data[i] && data[i] <= 90) {
+			++cnt;
+			operands[++top] = nums[cnt];
+		}
+		else {
+			if (data[i] == '+') {
+				//더하기
+				tmp = atof(operands[top - 1]) + atof(operands[top]);
+				top--;
+				operands[top] = tmp;
+			}
+			else if (data[i] == '-') {
+				tmp = atof(operands[top - 1]) - atof(operands[top]);
+				top--;
+				operands[top] = tmp;
+			}
+			else if (data[i] == '/') {
+				//나누기
+				tmp = atof(operands[top - 1]) / atof(operands[top]);
+				top--;
+				operands[top] = tmp;
+			}
+			else {
+				//곱하기
+				tmp = atof(operands[top - 1]) * atof(operands[top]);
+				top--;
+				operands[top] = tmp;
+			}
+		}
+		for (int i = 0; i < strlen(operands); i++) {
+			printf("%.2f ", (float)(operands[i]));
+		}
+		printf("\n");
+	}
+	printf("%.2f", (float)operands[0]);
+}
