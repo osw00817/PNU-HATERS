ed6be21db5d38144aac2d34a0d0dc94f9e9444b1
Kim jimin <dkjm0318@gmail.com>
2025-01-21 20:42:56 +0900

    🐛fix: BOJ_9935_문자열 폭발 버그 수정

--- Jimin-K04/week05/BOJ_9935_문자열폭발.c
+++ Jimin-K04/week05/BOJ_9935_문자열폭발.c
@@ -1,22 +1,62 @@
+/*
+store bomb_string
+use stack.
+
+If the incoming char matches the first char of the bomb_sring, subsequent char are checked until they differ from the bomb_string.
+If a different char appears, the check loop breaks.
+
+When the string match the bomb_string completely, pop is executed for the length of the bomb_string
+
+If the bomb_string is not found during a single cycle, the loop terminates.
+
+*/
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#define MAX 10001
+#include <string.h>
+#include <stdlib.h>
+#define MAX 1000001
+
+char* stack;
+int top = -1;
+
+void push(char x) {
+	stack[++top] = x;
+}
+
+void pop(void) {
+	top--;
+}
+
+int check_same_string(char* str_start, char* str, char* bomb_str) {
+
+	for (char* bomb = bomb_str + strlen(bomb_str) - 1; bomb >= bomb_str;) {
+		if (str < str_start || *str != *bomb) return 0;
+		str--;
+		bomb--;
+	}
+	return 1;
+}
 int main(void)
 {
-	int idx = 0;
-	char str[MAX], bomb[37], result[MAX];
+	char bomb_string[37];
+	char input_string[MAX];
 
-	scanf("%s %s", str, bomb);
+	scanf("%s %s", input_string, bomb_string);
 
-	for (int i = 0; str[i]; i++) {
-		result[idx++] = str[i]; //result 스택에 값 넣고
-		for (int j = 0; bomb[j]; j++) {
-			if (result[idx-1] == bomb[j]) {
-				idx--; //폭발 문자이면 pop 하기
-				break;
-			}
+	int string_len = strlen(input_string);
+	int bomb_str_len = strlen(bomb_string);
+
+	stack = malloc(sizeof(char) * (string_len + 1));
+
+	for (int i = 0; i < string_len; i++) {
+		push(input_string[i]);
+
+		if (check_same_string(stack, &stack[top], bomb_string)) {
+			for (int i = 0; i < bomb_str_len; i++) pop();
 		}
 	}
-	result[idx] = '\0';
-	if (idx > 0) printf("%s", result);
-	else printf("FRULA");
+	push('\0');
+
+	if (*stack == NULL) printf("FRULA");
+	else printf("%s", stack);
 }
